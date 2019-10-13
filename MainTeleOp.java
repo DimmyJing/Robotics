0c9a4eadd9338213a53e3a5587cb7bec3d1ad5c8
VidithM <vidhmadh1892@gmail.com>
2019-10-13 14:10:26 -0500

    Update MainTeleOp.java

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.Arrays;
+import java.lang.reflect.Array;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -11,12 +13,13 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import java.util.Arrays;
 
 @TeleOp(name="MainTeleOp", group="Linear Opmode")
 
-class MainTeleOp extends LinearOpMode{
+public class MainTeleOp extends LinearOpMode{
 
-  private DCMotor FL, FR, BL, BR;
+  private DcMotor FL, FR, BL, BR;
   private Gamepad g1, g2;
 
 
@@ -26,17 +29,23 @@ class MainTeleOp extends LinearOpMode{
     FR = hardwareMap.get(DcMotor.class, "FR");
     BL = hardwareMap.get(DcMotor.class, "BL");
     BR = hardwareMap.get(DcMotor.class, "BR");
+    
     g1 = gamepad1;
     g2 = gamepad2;
+    double[] in = {0,0};
 
     waitForStart();
 
     while(opModeIsActive()){
       //Divide by 4.15 because sin(x) + cos(x) has a max of +/-4.15. Want to scale down max power to +/-1.
-      FL.setPower((g1.right_stick_x + g1.right_stick_y)/4.15);
-      FR.setPower((g1.right_stick_x - g1.right_stick_y)/4.15);
-      BL.setPower((g1.right_stick_y - g1.right_stick_x)/4.15);
-      BR.setPower((-g1.right_stick_y - g1.right_stick_x)/4.15);
+      in[0] = g1.right_stick_x;
+      in[1] = g1.right_stick_y;
+      FL.setPower((in[0] - in[1])/4.15);
+      FR.setPower((in[1] + in[0])/4.15);
+      BL.setPower((-in[1] - in[0])/4.15);
+      BR.setPower((in[1] - in[0])/4.15);
+      telemetry.addData("g1 stats: ", Arrays.toString(in));
+      telemetry.update();
 
 
     }
