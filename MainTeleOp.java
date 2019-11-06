20620906c79cfaaefd5ae8859fed99fe764a95ab
VidithM <vidhmadh1892@gmail.com>
2019-11-05 23:23:55 -0600

    Update MainTeleOp.java

--- MainTeleOp.java
+++ MainTeleOp.java
@@ -17,135 +17,127 @@ import java.util.Arrays;
 
 @TeleOp(name="MainTeleOp", group="Linear Opmode")
 
-public class MainTeleOp extends LinearOpMode{
+public class MainTeleOp extends LinearOpMode {
+    private class ToggleVal {
+        private boolean prevState;
+        private boolean state;
 
-  private DcMotor FL, FR, BL, BR;
-  private Gamepad g1, g2;
-  private int pwrToggleCnt = 0;
-  private int servoCnt = 0;
+        public ToggleVal() {
+            prevState = false;
+            state = false;
+        }
 
-  private boolean precise = false;
-  private boolean servoDn = false;
+        public boolean update(boolean nextState) {
+            if (nextState && prevState != nextState)
+                state = !state;
+            prevState = nextState;
+            return state;
+        }
 
-  private Servo swingL, swingR;
-  private double[] in = {0,0};
+        public boolean getState() {
+            return state;
+        }
+    }
 
-  public void runOpMode(){
+    private DcMotor FL, FR, BL, BR, pivoL, pivoR;
+    private ToggleVal powerToggle;
+    private ToggleVal servoToggle;
 
-    FL = hardwareMap.get(DcMotor.class, "FL");
-    FR = hardwareMap.get(DcMotor.class, "FR");
-    BL = hardwareMap.get(DcMotor.class, "BL");
-    BR = hardwareMap.get(DcMotor.class, "BR");
+    private Servo swingL, swingR;
+    private double[] in = {0,0};
 
-    swingL = hardwareMap.get(Servo.class, "swingL");
-    swingR = hardwareMap.get(Servo.class, "swingR");
+    public MainTeleOp() {
+        powerToggle = new ToggleVal();
+        servoToggle = new ToggleVal();
+    }
 
-    g1 = gamepad1;
-    g2 = gamepad2;
+    public void runOpMode() {
+        FL = hardwareMap.get(DcMotor.class, "FL");
+        FR = hardwareMap.get(DcMotor.class, "FR");
+        BL = hardwareMap.get(DcMotor.class, "BL");
+        BR = hardwareMap.get(DcMotor.class, "BR");
+        pivoL = hardwareMap.get(DcMotor.class, "pivotL");
+        pivoR = hardwareMap.get(DcMotor.class, "pivotR");
 
+        swingL = hardwareMap.get(Servo.class, "swingL");
+        swingR = hardwareMap.get(Servo.class, "swingR");
 
-    waitForStart();
+        telemetry.addData("Welcome Drivers. Operate me Well", null);
+        telemetry.update();
 
-    while(opModeIsActive()){
+        waitForStart();
 
-      normalOps(); // need seperate method to continue normal functions in embedded while loops
-      /*
-      while(gamepad1.right_stick_y != 0){
-      normalOps();
-      pivotMotor.setPower(gamepad1.right_stick_y);
-      pivoted = true;
+        while(opModeIsActive()){
 
-      }
-
-      if(pivoted){
-          int dtheta = encoderticks;
-          articulating.setTargetPosition(-detheta? * revRatio);
-          articulating.setPower((dtheta/abs(dtheta)) * 0.2);
-          boolean interrupted = false;
-          while(articulating.isBusy()){
             normalOps();
-            if(gamepad1.right_stick_y != 0){
-            interrupted = true;
-            articulating.setPower(0);
-            break;
-          }
+            pivoL.setPower(gamepad2.left_stick_y/3);
+            pivoR.setPower(-gamepad2.left_stick_y/3);
+
+            /*
+                while(gamepad1.right_stick_y != 0){
+                    normalOps();
+                    pivotMotor.setPower(gamepad1.right_stick_y);
+                    pivoted = true;
+                }
+                if(pivoted){
+                    int dtheta = encoderticks;
+                    articulating.setTargetPosition(-detheta? * revRatio);
+                    articulating.setPower((dtheta/abs(dtheta)) * 0.2);
+                    boolean interrupted = false;
+                    while(articulating.isBusy()){
+                        normalOps();
+                        if(gamepad1.right_stick_y != 0){
+                            interrupted = true;
+                            articulating.setPower(0);
+                            break;
+                        }
+                    }
+                    if(!interrupted){
+                        articulating.setPower(0);
+                        pivotMotor.resetEncoder();
+                        articulating.resetEncoder();
+                    }
+                }
+            */
+
         }
-        if(!interrupted){
-        articulating.setPower(0);
-        pivotMotor.resetEncoder();
-        articulating.resetEncoder();
-      }
-      }
-
-      */
-      telemetry.addData("pwrToggle: ", precise);
-      telemetry.addData("g1 stats: ", Arrays.toString(in));
-      telemetry.update();
-
-
-    }
     }
 
-    public void normalOps(){
-      in[0] = g1.left_stick_x;
-      in[1] = g1.left_stick_y;
+    public void normalOps() {
+        in[0] = gamepad1.left_stick_x;
+        in[1] = gamepad1.left_stick_y;
 
-      if(g1.a){
-        pwrToggleCnt++;
-        if(pwrToggleCnt > 150){
-          pwrToggleCnt = 0;
-        }
-      } else {
+        boolean precise = powerToggle.update(gamepad1.a);
 
-      if(pwrToggleCnt != 0){
-        precise = !precise;
-        pwrToggleCnt = 0;
-      }
-
-      if(g1.right_trigger > 0 || g1.left_trigger > 0){
-        FL.setPower(g1.right_trigger - g1.left_trigger);
-        BL.setPower(g1.right_trigger - g1.left_trigger);
-        FR.setPower(g1.right_trigger - g1.left_trigger);
-        BR.setPower(g1.right_trigger - g1.left_trigger);
-      } else {
-
-        if(!precise){
-        //Fast mode
-        FL.setPower(in[0] - in[1]);
-        FR.setPower((in[0] + in[1]));
-        BL.setPower((-in[0] - in[1]));
-        BR.setPower((-in[0] + in[1]));
+        if (gamepad1.right_trigger + gamepad1.left_trigger > 0){
+            FL.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+            BL.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+            FR.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+            BR.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
         } else {
-        //Precise mode
-        FL.setPower((in[0] - in[1])/4);
-        FR.setPower((in[0] + in[1])/4);
-        BL.setPower((-in[0] - in[1])/4);
-        BR.setPower((-in[0] + in[1])/4);
+            if(precise){
+                telemetry.addData("Drive Mode: ", "PRECISION");
+                FL.setPower((in[0] - in[1])/4);
+                FR.setPower((in[0] + in[1])/4);
+                BL.setPower((-in[0] - in[1])/4);
+                BR.setPower((-in[0] + in[1])/4);
+            } else {
+                telemetry.addData("Drive Mode: ", "SPEED");
+                FL.setPower(in[0] - in[1]);
+                FR.setPower((in[0] + in[1]));
+                BL.setPower((-in[0] - in[1]));
+                BR.setPower((-in[0] + in[1]));
+            }
         }
-      }
+        if (servoToggle.update(gamepad1.left_bumper)) {
+            telemetry.addData("Servo Position: ", "DOWN");
+            swingL.setPosition(0.978);
+            swingR.setPosition(0.725);
+        } else {
+            telemetry.addData("Servo Position: ", "UP");
+            swingL.setPosition(0.8);
+            swingR.setPosition(0.6);
+        }
+        telemetry.update();
     }
-    if(g2.left_bumper) {
-      servoCnt++;
-      if(servoCnt > 150){
-        servoCnt = 0;
-      }
-    } else {
-
-      if(servoCnt != 0){
-        servoDn = !servoDn;
-        servoCnt = 0;
-      }
-
-      if(servoDn){
-        swingL.setPosition(1);
-        swingR.setPosition(0.8);
-      } else {
-        swingL.setPosition(0);
-        swingR.setPosition(0);
-      }
-    }
-
-  }
-
-
 }
